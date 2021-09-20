075093a2dcde03f2b15bee34f62b9b427b88871c
edwin <ferawkrane@outlook.com>
2021-09-20 12:13:35 -0500

    correo

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,7 +21,7 @@ class Welcome extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
 		$this->load->model("Login_model","lg");
-		$this->load->library('email');
+		$this->load->library('correo');
 
 
 	}
@@ -103,62 +103,8 @@ class Welcome extends CI_Controller {
 
 	}
 	public function recibirparametrosregistrocorreo(){
-		//Indicamos el protocolo a utilizar
-        $config['protocol'] = 'smtp';
-         
-       //El servidor de correo que utilizaremos
-        $config["smtp_host"] = 'ssl://mail.blazar.com.mx';
-         
-       //Nuestro usuario
-        $config["smtp_user"] = 'pruebas@blazar.com.mx';
-         
-       //Nuestra contraseña
-        $config["smtp_pass"] = 'a.HD7pV?C_Wf';   
-         
-       //El puerto que utilizará el servidor smtp
-        $config["smtp_port"] = '465';
-        
-       //El juego de caracteres a utilizar
-        $config['charset'] = 'utf-8';
- 
-       //Permitimos que se puedan cortar palabras
-        $config['wordwrap'] = TRUE;
-         
-       //El email debe ser valido 
-       $config['validate'] = true;
-       
-        
-      //Establecemos esta configuración
-        $this->email->initialize($config);
-  //Ponemos la dirección de correo que enviará el email y un nombre
-  $this->email->from('pruebas@blazar.com.mx', 'MANUEL RAZ');
-         
-  /*
-   * Ponemos el o los destinatarios para los que va el email
-   * en este caso al ser un formulario de contacto te lo enviarás a ti
-   * mismo
-   */
-  $correosalida=$this->input->post("email",true);
-	$this->email->to(trim($correosalida), 'EJEMPLO');
-	 
-  //Definimos el asunto del mensaje
-	$this->email->subject("CORREO TEST");
-	 
-  //Definimos el mensaje a enviar
-  $token=rand(0,1999999);
-	$this->email->message("TOKEN: {$token}");
-	 
-	//Enviamos el email y si se produce bien o mal que avise con una flasdata
-	if($this->email->send()){
-		echo "OK";
-		$this->session->set_flashdata('envio', 'Email enviado correctamente');
-	}else{
-		echo "ERROR";
-
-		$this->session->set_flashdata('envio', 'No se a enviado el email');
-	}
-	 
-
+$correo=$this->input->post("email",true);
+		$this->correo->enviar_correo($correo);
 	}
 
 
